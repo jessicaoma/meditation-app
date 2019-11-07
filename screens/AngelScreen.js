18513b90d75b884d6b6e861346a8db5f053e218d
Jessica <jessica.omana.b@gmail.com>
2019-11-07 10:26:39 -0600

    #61 Diseño gráfico de la carta seleccionada (anverso y reverso) con el texto que debe aparecer.

--- screens/AngelScreen.js
+++ screens/AngelScreen.js
@@ -1,23 +1,126 @@
-import React, {Component} from 'react';
-import {Button, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react'
+import { Animated, View, StyleSheet, Image, Dimensions, ScrollView } from 'react-native'
+
+const deviceWidth = Dimensions.get('window').width
+const FIXED_BAR_WIDTH = 50
+const BAR_SPACE = 10
+
+const images = [
+  'http://okoconnect.com/karim/images/angel1.png',
+  'http://okoconnect.com/karim/images/angelreve1.png',
+]
 
 export default class AngelScreen extends Component {
+ 
+  numItems = images.length
+  itemWidth = (FIXED_BAR_WIDTH / this.numItems) - ((this.numItems - 1) * BAR_SPACE)
+  animVal = new Animated.Value(0)
+
   static navigationOptions = {
     title: 'Tu ángel',
   };
-
-  _handleClick = () => {
-    //alert('This is a button!');
-    //this.props.navigation.navigate('Paso');
-  };
-
+  
   render() {
+    let imageArray = []
+    let barArray = []
+    images.forEach((image, i) => {
+      console.log(image, i)
+      const thisImage = (
+        <Image
+          key={`image${i}`}
+          source={{uri: image}}
+          style={{ width: deviceWidth }}
+        />
+      )
+      imageArray.push(thisImage)
+
+      const scrollBarVal = this.animVal.interpolate({
+        inputRange: [deviceWidth * (i - 1), deviceWidth * (i + 1)],
+        outputRange: [-this.itemWidth, this.itemWidth],
+        extrapolate: 'clamp',
+      })
+
+      const thisBar = (
+        <View
+          key={`bar${i}`}
+          style={[
+            styles.track,
+            {
+              width: this.itemWidth,
+              marginLeft: i === 0 ? 0 : BAR_SPACE,
+            },
+          ]}
+        >
+          <Animated.View
+
+            style={[
+              styles.bar,
+              {
+                width: this.itemWidth,
+                transform: [
+                  { translateX: scrollBarVal },
+                ],
+              },
+            ]}
+          />
+        </View>
+      )
+      barArray.push(thisBar)
+    })
+
     return (
-      <>
-        <Text>Angel</Text>
-      </>
-    );
+      <View
+        style={styles.container}
+        flex={1}
+      >
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={10}
+          pagingEnabled
+          onScroll={
+            Animated.event(
+              [{ nativeEvent: { contentOffset: { x: this.animVal } } }]
+            )
+          }
+        >
+
+          {imageArray}
+
+        </ScrollView>
+        <View
+          style={styles.barContainer}
+        >
+          {barArray}
+        </View>
+      </View>
+    )
   }
 }
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  barContainer: {
+    position: 'absolute',
+    zIndex: 2,
+    top: 40,
+    flexDirection: 'row',
+  },
+  track: {
+    backgroundColor: '#ccc',
+    overflow: 'hidden',
+    height: 2,
+  },
+  bar: {
+    backgroundColor: '#303030',
+    height: 2,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+})
