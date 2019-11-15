f092122b2be9284413d03befdd89acc8d1dee26b
Jessica <jessica.omana.b@gmail.com>
2019-11-14 18:20:15 -0600

    Componentes graficos de los pasos de un viaje, referencia http://okoconnect.com/karim/viaje-que-es-ser-feliz.html

--- screens/ViajeScreen.js
+++ screens/ViajeScreen.js
@@ -1,24 +1,148 @@
 import React, {Component} from 'react';
-import {Button, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity, Text, StyleSheet, FlatList, ScrollView, View} from 'react-native';
+import Button from '../components/Buttom';
+import ScreenBg from '../components/screenBg';
+import ItemBubbleLine from '../components/ItemBubbleLine';
+import Colors from '../constants/Colors';
+import Dims from '../constants/Dimensions';
+var viaje = {};
 
 export default class ViajeScreen extends Component {
   static navigationOptions = {
-    title: 'Viaje',
+    title: '¿Qué es ser Feliz?',
   };
 
   _handleClick = () => {
     //alert('This is a button!');
     this.props.navigation.navigate('Paso');
   };
-
-  render() {
+  renderItem = ({item}) => {
     return (
       <>
-        <Text>Viaje</Text>
-        <Button onPress={this._handleClick} title="Paso" />
+        {item.title !== undefined ? (
+          <ItemBubbleLine
+            color={viaje.color}
+            status={item.status}
+            onPress={this._goViaje}>
+            {item.title}
+          </ItemBubbleLine>
+        ) : (
+          <ItemBubbleLine
+            color={viaje.color}
+            status={item.status}
+            onPress={this._goViaje}>
+            {item.title}
+          </ItemBubbleLine>
+        )}
       </>
+    );
+  };
+  keyExtractor = item => item.id.toString() + 'viaje';
+  render() {
+    viaje = {
+      id: 1,
+      title: 'Ser Feliz',
+      viajes: [
+        {
+          id: 1,
+          title: 'Highlight',
+          status: 'done',
+        },
+        {
+          id: 2,
+          title: 'Teoría 1',
+          status: 'todo',
+        },
+        {
+          id: 3,
+          title: 'Reflexiones',
+          status: 'todo',
+        },
+        {
+          id: 4,
+          title: 'Ejercicio',
+          status: 'todo',
+        },
+        {
+          id: 5,
+          title: 'Recomendaciones',
+          status: 'todo',
+        },
+        {
+          id: 6,
+          title: 'Diario',
+          status: 'todo',
+        },
+        {
+          id: 7,
+          title: 'Cierre',
+          status: 'todo',
+        },
+      ],
+      color: this.props.navigation.getParam('bg', '#fdd58d'),
+      bgImg: 'http://okoconnect.com/karim/images/viaje-bg-2.png',
+    };
+    viaje.viajes.unshift({
+      id: 'cover',
+      source: {uri: viaje.cover},
+      color: viaje.color,
+      status: 'viajeTitle',
+      title: '¿Qué es ser feliz?'
+    });
+
+    return (
+      <>
+      <ScreenBg source={{uri: viaje.bgImg}} color={viaje.color}>
+      <View style={styles.statusBar} />
+        <ScrollView style={styles.container}>
+          
+          <FlatList
+              data={viaje.viajes}
+              renderItem={this.renderItem}
+              keyExtractor={this.keyExtractor}
+              style={styles.container}
+            />
+        </ScrollView>
+        <TouchableOpacity
+          onPress={this.handleLogin}
+          style={[styles.button]}>
+          <Text style={styles.buttonLabel}>Continuar mi viaje</Text>
+        </TouchableOpacity>
+      </ScreenBg>
+    </>
+      
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    height: '100%',
+  },
+  button: {
+    backgroundColor: Colors.second,
+    borderRadius: 30,
+    alignSelf: 'stretch',
+    width: '100%',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  buttonLabel: {
+    color: 'white',
+    fontSize: Dims.window.width * 0.041,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    letterSpacing: 1.5,
+    lineHeight: 50,
+    fontFamily: 'MyriadPro-Regular',
+  },
+});
