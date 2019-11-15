4cd8e8eb768534c910cc6b226ec4999c545a7b32
Marvin Gonzalez <marvingonzalez@okotecnologia.com>
2019-11-14 23:29:07 -0400

    Agregando imagenes del angel dentro de la app

--- components/ItemBubbleLine.js
+++ components/ItemBubbleLine.js
@@ -92,7 +92,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: Dims.regularSpace,
     borderColor: Colors.borderWhite,
     backgroundColor: 'white',
-    justifyContent: 'left',
+    //justifyContent: 'left',
     flexDirection: 'row',
     flex: 1,
   },
