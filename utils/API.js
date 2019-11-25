be6876fbaaf335f9c68ef6afa9a16f6e7b731990
Marvin Gonzalez <marvingonzalez@okotecnologia.com>
2019-11-24 21:31:40 -0400

    Las Categorias son consultadas desde el api

--- utils/API.js
+++ utils/API.js
@@ -1,159 +1,70 @@
-const BASE_API = 'http://apikarimtempletest.azurewebsites.net/api/';
+const BASE_API = 'http://okotesting-001.azurewebsites.net/api/';
+
+/**
+ * @typedef {Object} AngelMensaje
+ * @prop {string} id Identificador
+ * @prop {string} title Nombre del ángel
+ * @prop {string} sentence Mensaje del angel
+ *
+ * @typedef {Object} Categoria
+ * @prop {string} id Identificador
+ * @prop {string} title Título
+ * @prop {string} [cover] Imagen previa de la introcucción
+ * @prop {string} [media] Ruta del video introductorio
+ * @prop {string} [backgroundImage] Fondo de la categoria
+ * @prop {string} color Color caracteristico de la categoria
+ * @prop {string} itemImage Imagen para la lista
+ * @prop {string} [journeys] Lista de viajes hijos
+ *
+ * @typedef {Object} Meditación
+ * @prop {string} id Identificador
+ * @prop {string} title Título
+ * @prop {string} intro Video previo a la meditación
+ * @prop {string} media Ruta del audio correspondiente a la meditación
+ * @prop {string} backgroundImage Fondo de la meditación
+ * @prop {string} color Color caracteristico de la meditación
+ * @prop {string} itemImage Imagen para la lista
+ * @prop {string} isFree Lista de viajes hijos
+ *
+ * @typedef {Object} Audiolibro
+ * @prop {string} id Identificador
+ * @prop {string} title Título
+ * @prop {string} media Ruta del audio correspondiente al Audiolibro
+ * @prop {string} backgroundImage Fondo del Audiolibro
+ * @prop {string} color Color caracteristico del Audiolibro
+ * @prop {string} itemImage Imagen para la lista
+ * @prop {string} progress Indica hasta que punto se ha reproducido
+ * @prop {string} isFree Lista de viajes hijos
+ */
 
 class Api {
+  /** @return {Promise<Meditación[]>} */
   async getMeditaciones() {
-    //const query = await fetch(`${BASE_API}meditaciones`);
-    //const data = await query.json();
-    //return data;
-    return [
-      {
-        id: 'med1',
-        title: 'Meditación Básica',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar2-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar2.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med2',
-        title: 'Meditación 2',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar4-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar4.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med3',
-        title: 'Meditación 3',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar1-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar1.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med4',
-        title: 'Meditación 4',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar3-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar3.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med5',
-        title: 'Meditación 5',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar2-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar2.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med6',
-        title: 'Meditación 6',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar4-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar4.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med7',
-        title: 'Meditación 7',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar1-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar1.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med8',
-        title: 'Meditación 8',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar3-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar3.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med9',
-        title: 'Meditación 9',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar2-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar2.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'med10',
-        title: 'Meditación 10',
-        color: '#7883a4',
-        backgroundImage: 'http://okoconnect.com/karim/images/meditar4-full.png',
-        itemImage: 'http://okoconnect.com/karim/images/meditar4.png',
-        intro: 'http://okoconnect.com/karim/videos/pre_meditacion.mp4',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-    ];
+    const query = await fetch(`${BASE_API}meditaciones`);
+    const data = await query.json();
+    return data;
   }
 
+  /** @returns {Promise<Audiolibro[]>} */
   async getAudiolibros() {
-    /*const query = await fetch(`${BASE_API}audiolibros`);
+    const query = await fetch(`${BASE_API}audiolibros`);
     const data = await query.json();
-    return data;*/
-    return [
-      {
-        id: 'aud1',
-        title: 'La aventura espiritual',
-        itemImage: 'http://okoconnect.com/karim/images/libro3.png',
-        color: '#82d3ea',
-        backgroundImage: 'http://okoconnect.com/karim/images/libro3-.png',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'aud2',
-        title: 'Aprendiendo a Meditar',
-        itemImage: 'http://okoconnect.com/karim/images/libro2.png',
-        color: '#50628e',
-        backgroundImage: 'http://okoconnect.com/karim/images/libro2-.png',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-      {
-        id: 'aud3',
-        title: '101 Frases para reflexionar',
-        itemImage: 'http://okoconnect.com/karim/images/libro1.png',
-        color: '#ffffff',
-        backgroundImage: 'http://okoconnect.com/karim/images/libro1-.png',
-        media: 'http://okoconnect.com/karim/meditaciones/Meditacion-Basica.mp3',
-        isFree: true,
-      },
-    ];
+    return data;
   }
 
+  /** @return {Promise<AngelMensaje>} */
   async getAngelMessage() {
-    /*const query = await fetch(`${BASE_API}angelmessage`);
+    const query = await fetch(`${BASE_API}angelmessage`);
     const data = await query.json();
-    return data;*/
-    return {
-      id: 'mes1',
-      title: 'El ángel de la abundancia',
-      sentence:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-    };
+    return data;
+  }
+
+  /** @return {Promise<Categoria[]>} */
+  async getCategorias() {
+    const query = await fetch(`${BASE_API}categorias`);
+    const data = await query.json();
+    return data;
+    //return catDB;
   }
 }
 
