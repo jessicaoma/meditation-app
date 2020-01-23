97e7edc0d3577186f1cc32f6e9661ea66fb18f3d
Jessica <jessica@Jessicas-iMac.local>
2020-01-23 15:29:51 -0600

    Nueva pantalla de Crear una Cuenta, ajustes y mejoras al Login

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import MainTabNavigator from './MainNavigator';
 import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
+import CrearCuentaScreen from '../screens/CrearCuentaScreen';
 
 export default createAppContainer(
   createSwitchNavigator(
@@ -10,6 +11,7 @@ export default createAppContainer(
       App: MainTabNavigator,
       Splash: SplashScreen,
       Login: LoginScreen,
+      CrearCuenta: CrearCuentaScreen,
     },
     {
       initialRouteName: 'Splash',
---
+++ screens/CrearCuentaScreen.js
@@ -0,0 +1,171 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  StatusBar,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+import Colors from '../constants/Colors';
+import LogoFacebook from '../constants/LogoFacebook';
+import LogoGoogle from '../constants/LogoGoogle';
+import InputLogin from '../components/InputLogin';
+import Logo from '../components/Logo';
+import Dims from '../constants/Dimensions';
+import ScreenBg from '../components/screenBg';
+//TODO hacer todo el manejo
+/**
+ * @typedef Props
+ * @prop {import('react-navigation').NavigationScreenProp} navigation
+ * @extends {Component<Props>}
+ */
+export default class CrearCuentaScreen extends Component {
+  handleLogin = () => {
+    this.props.navigation.navigate('Login');
+  };
+  passwordRef = {};
+  goEmail = () => {
+    this.passwordRef.focus();
+  };
+  refPassword = input => {
+    this.passwordRef = input;
+  };
+  goPassword = () => {
+    this.passwordRef.focus();
+  };
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.flex1}>
+            <ScrollView contentInsetAdjustmentBehavior="automatic">
+                <ScreenBg
+                source={{uri: 'http://okoconnect.com/karim/images/crearcuenta-bg.png'}}
+                styleImage={{resizeMode: 'cover', height: Dims.window.height}}>
+                    <View style={styles.container}>
+                        <View style={styles.header}>
+                            <Text style={styles.welcomeTitle}>Crear cuenta con</Text>
+                            <View style={styles.sociallogos}>
+                              <TouchableOpacity onPress={this._changeIcon} style={{marginRight: 5}}><LogoFacebook /></TouchableOpacity>
+                              <TouchableOpacity onPress={this._changeIcon} style={{}}><LogoGoogle /></TouchableOpacity>
+                            </View>
+                            <Text style={styles.welcomeTitle}>ó registrarme vía email</Text>
+                        </View>
+                                
+
+                        <View style={styles.inputs}>
+                            <InputLogin
+                                placeholder="Nombre"
+                                type="text"
+                                onSubmitEditing={this.goEmail}
+                                blurOnSubmit={false}
+                            />
+                            <InputLogin
+                                placeholder="Correo"
+                                type="text"
+                                onSubmitEditing={this.goPassword}
+                                blurOnSubmit={false}
+                            />
+                            <InputLogin
+                                placeholder="Constraseña"
+                                type="password"
+                                onSubmitEditing={this.goPassword}
+                                inputRef={this.refPassword}
+                            />
+                            <InputLogin
+                                placeholder="Confirmar Constraseña"
+                                type="password"
+                                inputRef={this.refPassword}
+                            />
+                        </View>
+
+                        <View>
+                            <TouchableOpacity
+                                onPress={this.handleLogin}
+                                style={[styles.button]}>
+                                <Text style={styles.buttonLabel}>¡LISTO!</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScreenBg>
+            </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  flex1: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    padding: 40,
+    minHeight: Dims.window.height - 120
+  },
+  welcomeTitle: {
+    color: '#ABA0B5',
+    fontSize: Dims.paragraph,
+    lineHeight: 25,
+    fontFamily: 'MyriadPro-Regular',
+    letterSpacing: 1.2,
+    textAlign: 'center',
+  },
+  header: {
+    flex: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center',
+    marginBottom: 15
+  },
+  sociallogos: {
+    flex: 0,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    height: 30,
+  },
+  button: {
+    backgroundColor: Colors.second,
+    borderRadius: 30,
+    alignSelf: 'stretch',
+    width: '100%',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  buttonLabel: {
+    color: 'white',
+    fontSize: Dims.inputText,
+    textAlign: 'center',
+    lineHeight: 50,
+    fontFamily: 'MyriadPro-Regular',
+    width: '100%',
+    minWidth: '100%',
+  },
+  forgetText: {
+    color: Colors.gray,
+    fontSize: Dims.inputText,
+    lineHeight: 28,
+    marginVertical: Dims.smallSpace,
+  },
+  full: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    padding: 25,
+  },
+});
