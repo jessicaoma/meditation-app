58fba914ff310272580e3b211faec2cebf851872
Marvin Gonzalez <marvingonzalez@okotecnologia.com>
2020-04-01 19:37:09 -0400

    #90 - Revisar Pantallas para Android

--- navigation/ViajeNavigator.js
+++ navigation/ViajeNavigator.js
@@ -6,11 +6,6 @@ import PasoBScreen from '../screens/PasoBScreen';
 import PasoCScreen from '../screens/PasoCScreen';
 import PasoDScreen from '../screens/PasoDScreen';
 import PasoEScreen from '../screens/PasoEScreen';
-import PasoFScreen from '../screens/PasoFScreen';
-import PasoGScreen from '../screens/PasoGScreen';
-//import PasoH from '../screens/PasoH';
-import PasoI from '../screens/PasoI';
-// import PasoJ from '../screens/PasoJ';
 import PasoK from '../screens/PasoK';
 
 const ViajeNavigator = createStackNavigator(
@@ -21,12 +16,7 @@ const ViajeNavigator = createStackNavigator(
     PasoB: PasoBScreen,
     PasoC: PasoCScreen,
     PasoD: PasoDScreen,
-    // PasoE: PasoEScreen,
-    // PasoF: PasoFScreen,
-    // PasoG: PasoGScreen,
-    //PasoH: PasoH,
-    // PasoI,
-    // PasoJ,
+    PasoE: PasoEScreen,
     PasoK,
   },
   {
