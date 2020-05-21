1bc27e1981bf35789e781c4854f8eb51d1b2efed
Jessica <jessica@Jessicas-iMac.local>
2020-05-21 18:50:15 -0500

    viajes finalizados v1

--- screens/ViajesCompletadosScreen.js
+++ screens/ViajesCompletadosScreen.js
@@ -6,6 +6,7 @@ import {
   SafeAreaView,
   Image,
   FlatList,
+  TouchableOpacity
 } from 'react-native';
 import ItemBubble from '../components/ItemBubble';
 import Colors from '../constants/Colors';
@@ -15,6 +16,7 @@ import API, {user} from '../utils/API';
 import ScalableText from 'react-native-text';
 import {connect} from 'react-redux';
 import {HeaderBackButton} from '@react-navigation/stack';
+import LogoCursoDone from '../constants/LogoCursoDone';
 
 /**
  * @typedef Props
@@ -70,14 +72,20 @@ class ViajeCompletadosScreen extends Component {
           paddingHorizontal: Dimensions.regularSpace,
           paddingTop: index === 0 ? Dimensions.regularSpace : 0,
         }}>
-        <ItemBubble
-          color={item.color}
-          fill
-          onPress={() => {
-            this._handleClick(item);
-          }}>
-          {item.titulo}
-        </ItemBubble>
+        <TouchableOpacity onPress={() => { this._goViaje(index); }}>
+        <View style={styles.listItem}>
+          <View style={[styles.itemNumber, {borderColor: item.color}]}>
+            <ScalableText style={styles.itemNumberText}>{index + 1}.</ScalableText>
+          </View>
+          <View style={styles.infoSect}>
+                <View style={styles.tituloSection}>
+                    <View><ScalableText style={styles.titulo}>{item.titulo}</ScalableText></View>
+                    <View style={styles.statusIcon}><LogoCursoDone color={item.color} style={styles.statusIcon} /></View>
+                </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+
       </View>
     );
   };
@@ -162,6 +170,55 @@ const styles = StyleSheet.create({
     flex: 1,
     width: Dims.window.width,
     height: Dims.window.width * 0.8,
+  },
+  listItem: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 25,
+  },
+  infoSect: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingLeft: 10,
+    maxWidth: '100%',
+    flex: 1,
+  },
+  itemNumber: {
+    borderRightWidth: 2,
+    paddingRight: 5,
+  },
+  itemNumberText: {
+    fontFamily: 'MyriadPro-Semibold',
+    lineHeight: 22,
+    fontSize: 15,
+    color: '#85787B',
+  },
+  
+  tituloSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+    position: 'relative',
+    marginBottom: 3,
+  },
+  titulo: {
+    fontFamily: 'MyriadPro-Semibold',
+    lineHeight: 18,
+    fontSize: 17,
+    color: '#85787B',
+    letterSpacing: 1.06,
+    flex: 0.8, 
+    flexWrap: 'wrap',
+    paddingRight: 28,
+    marginTop: 5,
+  },
+  statusIcon:{
+    marginLeft: 5,
+    marginTop: -3,
+    position: 'absolute',
+    right: 0
   },
 });
 
