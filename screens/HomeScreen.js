471105dfb3328581831788b99749645a74c1886b
Jessica <jessica@Jessicas-iMac.local>
2020-06-01 17:44:00 -0500

    Nuevo logo Serenamente

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -55,7 +55,7 @@ class Home extends Component {
     headerStyle: {height: headerH},
     headerLeft: () => (
       <View style={{marginLeft: 16}}>
-        <Logo />
+        <Logo style={{marginTop: 20,}} />
       </View>
     ),
     headerRight: () => (
